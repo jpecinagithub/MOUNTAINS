af4abff4cff7d95f513d2375e5454edf5885f8ce
jpecina <jpecina@gmail.com>
2026-02-14 18:58:35 +0100

    Reorganizar iconos

---
+++ config.js
@@ -0,0 +1,8 @@
+// Copy to `config.js` (gitignored) and set your key.
+// You must enable billing and APIs in Google Cloud Console:
+// - Street View Static API
+// - Maps Static API
+//
+// IMPORTANT: Any key used in frontend code is public. Restrict it by HTTP referrer.
+window.GOOGLE_MAPS_API_KEY = "AIzaSyAbnYLPFy_E5O4xXHfHgIvbENDEM4MSW6Q";
+
